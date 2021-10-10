a78c36aa7477992438d0064b8b22dd56f6dddf0a
4nzi <zm17328@josai.ac.jp>
2021-10-10 18:02:44 +0900

    add purge setting

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: ['./src/pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
