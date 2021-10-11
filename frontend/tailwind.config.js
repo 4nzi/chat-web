021b2947255b5915aae2e764b2d05f1e65cbca59
4nzi <zm17328@josai.ac.jp>
2021-10-11 19:42:48 +0900

    add custom utilities

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,5 +8,36 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".btn": {
+          width: "100%",
+          backgroundColor: "#6366F1",
+          color: "#ffffff",
+          fontWeight: "700",
+          padding: "0.5rem 1rem",
+          borderRadius: "0.25rem",
+          transition: "all 0.2s ease"
+        },
+        ".btn:hover": {
+          backgroundColor: "#4338CA",
+        },
+        ".ipt": {
+          width: "100%",
+          backgroundColor: "#374151",
+          color: "#ffffff",
+          lineHeight: "1.25",
+          padding: "0.5rem 0.75rem",
+          borderWidth: "1px",
+          borderRadius: "0.25rem",
+          borderColor: "#000000",
+        },
+        ".ipt:focus": {
+          outline: "0",
+        }
+      };
+      addUtilities(newUtilities);
+    }
+  ]
 }
