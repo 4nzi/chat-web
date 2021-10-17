333cfea1397418ae4b2abcfa6b2025542a87a2a1
4nzi <zm17328@josai.ac.jp>
2021-10-18 04:38:23 +0900

    add Noresults component

---
+++ frontend/src/components/NoResults.tsx
@@ -0,0 +1,9 @@
+const NoResults: React.VFC = () => {
+  return (
+    <div className="translate-y-2/4 mx-auto text-gray-400">
+      <p>トークを開始しよう!!</p>
+    </div>
+  )
+}
+
+export default NoResults
