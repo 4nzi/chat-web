9e6529f878549150699239117662520a9bd38b7b
4nzi <zm17328@josai.ac.jp>
2021-10-18 03:48:32 +0900

    add session api for SSR

---
+++ frontend/src/pages/api/sessionLogout.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest as Req, NextApiResponse as Res } from 'next'
+
+import { parseCookies, destroyCookie } from 'nookies'
+import { firebaseAdmin } from '../../../firebaseAdminConfig'
+
+export default async function sessionLogoutApi(req: Req, res: Res) {
+  if (req.method !== 'POST') return res.status(404).send('Not Found')
+
+  const auth = firebaseAdmin.auth()
+  const sessionId = parseCookies({ req }).session || ''
+  const decodedClaims = await auth
+    .verifySessionCookie(sessionId)
+    .catch(() => null)
+
+  if (decodedClaims) {
+    await auth.revokeRefreshTokens(decodedClaims.sub)
+  }
+
+  // Cookieに保存されているセッションIDを削除
+  destroyCookie({ res }, 'session', { path: '/' })
+
+  res.send(JSON.stringify({ status: 'success' }))
+}
