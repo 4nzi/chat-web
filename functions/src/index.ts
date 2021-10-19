fdde0c86bdc477c0c77aa8806409bd154c352726
4nzi <zm17328@josai.ac.jp>
2021-10-20 05:40:04 +0900

    add createRooms function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,28 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+const db = admin.firestore()
+
+export const createRooms = functions.firestore
+  .document('rooms/{roomID}')
+  .onCreate(async (snap, context) => {
+    const value = snap.data()
+    const users: string[] = value.userIDs
+
+    for (const i of users) {
+      const personID = users.find((user) => user !== i)
+      const payload = {
+        personID: personID,
+        unreadCount: 0,
+        body: 'トークを開始しよう!!',
+        updatedAt: admin.firestore.FieldValue.serverTimestamp(),
+      }
+      await db
+        .collection('users')
+        .doc(i)
+        .collection('joiningRooms')
+        .doc(snap.id)
+        .set(payload)
+    }
+  })
